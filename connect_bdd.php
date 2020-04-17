32c689c6c1073a47506a8b01befb40e5b0c7de1e
kevin <kevin.fanton@hotmail.com>
2020-04-17 10:34:15 +0200

    page de conexion

---
+++ connect_bdd.php
@@ -0,0 +1,12 @@
+<?php
+	$servername = 'mysql:host=localhost;dbname=gestioncommande';
+	$username ='root';
+	$password='';
+
+
+	try{
+		$base = new PDO($servername, $username, $password);
+	}
+	catch(exception $e){
+		die ('Erreur :' .$et->getMessage());
+	}
