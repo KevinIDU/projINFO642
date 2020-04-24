cae755166e8c5f022f03fe47fb9c478fd71e1fc0
YoannRaguenes <58259678+YoannRaguenes@users.noreply.github.com>
2020-04-24 18:24:47 +0200

    Add files via upload

---
+++ accEtudiant.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+	
+       <meta charset="utf-8">
+        <link rel="stylesheet" href="accEtudiant.css"  type="text/css" />
+    </head>
+    <body>
+	
+		<div id="entete">
+		
+			<div id="cadreConnec">
+			Nom d'utilisateur
+			<p>Se déconnecter</p>
+			</div>
+			
+		
+		</div>
+
+		<div id="main">
+			<div id="menu">
+			<input type="submit" id='submit' value='FAIRE UNE DEMANDE' >
+			<input type="submit" id='submit' value='MES DEMANDES' >
+			<input type="submit" id='submit' value='CONTACTER UN ENSEIGNANT' >
+			<input type="submit" id='submit' value='ENQUETE DE SATISFACTION' >
+			</div>
+
+		<div id="contenu">
+		Version Etudiant
+		<img src="images\logoPOPO.jpg" alt="" />
+		</div>
+		</div>
+
+		<div id="footer">
+		Site réalisé par Adrien Simard, Roshan Nepaul, Kévin Fanton et Yoann Raguenes
+		<p> Etudiants en 3ème année de la filière IDU de Polytech Annecy-Chambéry</p>
+			
+		</div>
+	</body>
+</html>	
