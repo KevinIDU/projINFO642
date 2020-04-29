e1fbf47f0c025005d0517a58f6e037f2eec484ad
Nepaul-roshan <56953229+Nepaul-roshan@users.noreply.github.com>
2020-04-29 11:40:16 +0200

    Add files via upload
    Ajout des demandes et consultations des demandes

--- page_1.php
+++ page_1.php
@@ -11,13 +11,18 @@
 	<h2 id="titre">Ajout d'une demande de matériel</h2>
 	<div  id="ajout">
 		<div id="form">
+			<?php
+			session_start();
+			include("connexion_base.php");
+			?>
 			<form action = 'ajout_demande.php' method='post'>
 				<fieldset>
-					<legend>Ajout Demande</legend>
+					<legend>Ajout Demande</legend>					
+					<p><label>Numéro étudiant</label> : <input type='text' name='numero' /></p>
 					<p><label>Description du projet</label> : <textarea type='text' name='description'></textarea></p>
 					<p><label>Ligne budgétaire</label> : <input type='number' name='budget' /></p>
 					<p><label>Produits concernés</label> : <input type='text' name='produits' /></p>
-					<p><label>Quantité</label> : <input type='number' name='quantité' /></p>
+					<p><label>Quantité</label> : <input type='text' name='quantite' /></p>
 					<p><label>Fournisseur</label> : <input type='text' name='fournisseur' /></p>
 					<p><label>Prix unitaire</label> : <input type='text' name='prix' /></p>
 					<p><label>Lien vers site marchand</label> : <input type='url' name='lien' /></p>
