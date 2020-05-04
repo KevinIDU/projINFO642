8e36e0991193711bd509dd1165c3e640879ed41d
YoannRaguenes <58259678+YoannRaguenes@users.noreply.github.com>
2020-05-04 15:30:11 +0200

    Add files via upload

--- complete_demande.php
+++ complete_demande.php
@@ -1,8 +1,11 @@
+
+
+
 <html>
     <head>
 	
        <meta charset="utf-8">
-        <link rel="stylesheet" href="accEtudiant.css"  type="text/css" />
+        <link rel="stylesheet" href="complete_demande.css"  type="text/css" />
     </head>
     <body>
 	
@@ -35,12 +38,45 @@
 			
 
 		<div id="contenu">
-			Version Enseignant
-			<div id="logopopo">
-			<img src="images\logoPOPO.jpg" alt="" />
-		</div>	
+			<div id="formulaire1">
+			</style>
+			
+
+			<div id="modif">
+			<body id="titre">
+				
+			<h2>Complement des demandes</h2>	
+			<?php
+			session_start();
+			$nom = "CIMPAN";
+			include("connect_bdd.php");
+			$sql =  "SELECT *
+			FROM demande_materiel WHERE enseignant = '$nom'";
+			$sth = $base->prepare($sql);
+			$sth->execute();
+			$result = $sth->fetchAll();
+			echo "<form action = 'complete.php' method='post'>";
+			echo "<label>Demandes</label> : <select name='demande'>";
+			foreach ($result as $row) {
+			echo "<option>".$row['description'];			}
+			echo "</select>"; 
+			?>
+			</div>
+
+			<div id="consulter1">
+
+			<?php
+			echo "<input type='submit1' id='bouton_consul' value='Consulter'>";
+			echo "</form>";	
+
+			?>
+			</div>
+			</div>
+			
 		</div>
-		</div>
+	</div>
+
+
 
 		<div id="footer">
 		Site réalisé par Adrien Simard, Roshan Nepaul, Kévin Fanton et Yoann Raguenes
